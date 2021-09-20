051f4c5ab84fa7f6edf9ed40cca367124ec5d409
Faizan Ahmed <fahmed.raza@viftech.com.pk>
2021-09-20 21:27:28 +0500

    commit live chat 20092021

--- resources/views/website/layouts/master.blade.php
+++ resources/views/website/layouts/master.blade.php
@@ -50,10 +50,10 @@
 @include('website.layouts.footer')
 <!-- main-footer end -->
 
-{{--<!--Scroll to top-->--}}
-{{--<button class="scroll-top scroll-to-target" data-target="html">--}}
-{{--    <span class="fa fa-arrow-up"></span>--}}
-{{--</button>--}}
+<!--Scroll to top-->
+<button class="scroll-top scroll-to-target" data-target="html">
+    <span class="fa fa-arrow-up"></span>
+</button>
 
 
 <!-- jequery plugins -->
@@ -71,17 +71,17 @@
 
 <!-- main-js -->
 <script src="/assets/website/js/script.js"></script><!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/6147304c25797d7a89ffbccc/1ffv19b39';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
+{{--<script type="text/javascript">--}}
+{{--    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();--}}
+{{--    (function(){--}}
+{{--        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];--}}
+{{--        s1.async=true;--}}
+{{--        s1.src='https://embed.tawk.to/6147304c25797d7a89ffbccc/1ffv19b39';--}}
+{{--        s1.charset='UTF-8';--}}
+{{--        s1.setAttribute('crossorigin','*');--}}
+{{--        s0.parentNode.insertBefore(s1,s0);--}}
+{{--    })();--}}
+{{--</script>--}}
 <!--End of Tawk.to Script-->
 @stack('scripts')
 </body><!-- End of .page_wrapper -->
