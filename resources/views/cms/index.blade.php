7411a11a7dc03ff9bd59dbde4d0ee19c151dc8e6
unknown <fahmed.raza@viftech.com.pk>
2021-09-30 21:48:02 +0500

    pull changes

--- resources/views/cms/index.blade.php
+++ resources/views/cms/index.blade.php
@@ -5,26 +5,40 @@
         .card .card-body {
             height: 125px;
         }
+        #afterimage::before{
+            content: "";
+            background: url('{{asset('assets/images/dashboard-pic.jpg')}}');
+            object-fit: cover;
+            max-width: 100%;
+            height: 100vh;
+            background-size: cover;
+            position: absolute;
+            top: 0px;
+            right: 0px;
+            bottom: 0px;
+            left: 0px;
+            opacity: 0.72;
+        }
     </style>
 @endpush
 
 @section('content')
     <main>
-        <div class="container-fluid site-width">
+        <div class="container-fluid site-width" >
             <!-- START: Card Data-->
             <div class="row">
                 <div class="col-12 mt-3">
-                    <div class="card">
-                        <div class="card-header  justify-content-between align-items-center">
-                            <h4 class="card-title">Dashboard</h4>
+                    <div id="afterimage" class="card">
+                        <div class="card-header border-0 justify-content-between align-items-center">
+                            <h4 class="card-title opacity-100 font-weight-bold">Dashboard</h4>
                         </div>
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
-                                    <div class="card cstRedColor">
+                                    <div class="card cstRedColor border-bottom border-success border-w-5 border-bottom zoom">
                                         <div class="card-body">
                                             <div class='d-flex px-0 px-lg-2 py-2 align-self-center'>
-                                                <i class="fas fa-user-check fa-3x fa-fw mt-2 text-white"></i>
+                                                <i class="fas fa-user-check fa-2x fa-fw mt-2 text-white"></i>
                                                 <div class='card-liner-content'>
                                                     <div class="media-body align-self-center text-white">
                                                         <span class="mb-0 h5 font-w-600">Active
@@ -38,10 +52,10 @@
                                 </div>
 
                                 <div class="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
-                                    <div class="card cstRedColor">
+                                    <div class="card cstRedColor border-bottom border-success border-w-5 border-bottom zoom">
                                         <div class="card-body">
                                             <div class='d-flex px-0 px-lg-2 py-2 align-self-center'>
-                                                <i class="fas fa-user-alt-slash fa-3x fa-fw mt-2 text-white"></i>
+                                                <i class="fas fa-user-alt-slash fa-2x fa-fw mt-2 text-white"></i>
                                                 <div class='card-liner-content'>
                                                     <div class="media-body align-self-center text-white">
                                                         <span class="mb-0 h5 font-w-600">Block
@@ -55,10 +69,10 @@
                                 </div>
 
                                 <div class="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
-                                    <div class="card cstRedColor">
+                                    <div class="card cstRedColor border-bottom border-success border-w-5 border-bottom zoom">
                                         <div class="card-body">
                                             <div class='d-flex px-0 px-lg-2 py-2 align-self-center'>
-                                                <i class="fas fa-user-check fa-3x fa-fw mt-2 text-white"></i>
+                                                <i class="fas fa-user-check fa-2x fa-fw mt-2 text-white"></i>
                                                 <div class='card-liner-content'>
                                                     <div class="media-body align-self-center text-white">
                                                         <span class="mb-0 h5 font-w-600">Admins</span><br>
@@ -71,10 +85,10 @@
                                 </div>
 
                                 <div class="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
-                                    <div class="card cstRedColor">
+                                    <div class="card cstRedColor border-bottom border-success border-w-5 border-bottom zoom">
                                         <div class="card-body">
                                             <div class='d-flex px-0 px-lg-2 py-2 align-self-center'>
-                                                <i class="fas fa-user-check fa-3x fa-fw mt-2 text-white"></i>
+                                                <i class="fas fa-user-check fa-2x fa-fw mt-2 text-white"></i>
                                                 <div class='card-liner-content'>
                                                     <div class="media-body align-self-center text-white">
                                                         <span class="mb-0 h5 font-w-600">Students</span><br>
@@ -87,10 +101,10 @@
                                 </div>
 
                                 <div class="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
-                                    <div class="card cstRedColor">
+                                    <div class="card cstRedColor border-bottom border-success border-w-5 border-bottom zoom">
                                         <div class="card-body">
                                             <div class='d-flex px-0 px-lg-2 py-2 align-self-center'>
-                                                <i class="fas fa-user-check fa-3x fa-fw mt-2 text-white"></i>
+                                                <i class="fas fa-user-check fa-2x fa-fw mt-2 text-white"></i>
                                                 <div class='card-liner-content'>
                                                     <div class="media-body align-self-center text-white">
                                                         <span class="mb-0 h5 font-w-600">Researchers</span><br>
@@ -103,10 +117,10 @@
                                 </div>
 
                                 <div class="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
-                                    <div class="card cstRedColor">
+                                    <div class="card cstRedColor border-bottom border-success border-w-5 border-bottom zoom">
                                         <div class="card-body">
                                             <div class='d-flex px-0 px-lg-2 py-2 align-self-center'>
-                                                <i class="fas fa-check fa-3x fa-fw mt-2 text-white"></i>
+                                                <i class="fas fa-check fa-2x fa-fw mt-2 text-white"></i>
                                                 <div class='card-liner-content'>
                                                     <div class="media-body align-self-center text-white">
                                                         <span class="mb-0 h5 font-w-600 text-break">Fyp Proposals(Approved)</span><br>
@@ -119,10 +133,10 @@
                                 </div>
 
                                 <div class="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
-                                    <div class="card cstRedColor">
+                                    <div class="card cstRedColor border-bottom border-success border-w-5 border-bottom zoom">
                                         <div class="card-body">
                                             <div class='d-flex px-0 px-lg-2 py-2 align-self-center'>
-                                                <i class="fas fa-times fa-3x fa-fw mt-2 text-white"></i>
+                                                <i class="fas fa-times fa-2x fa-fw mt-2 text-white"></i>
                                                 <div class='card-liner-content'>
                                                     <div class="media-body align-self-center text-white">
                                                         <span class="mb-0 h5 font-w-600 text-break">Fyp Proposals(Rejected)</span><br>
@@ -135,10 +149,10 @@
                                 </div>
 
                                 <div class="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
-                                    <div class="card cstRedColor">
+                                    <div class="card cstRedColor border-bottom border-success border-w-5 border-bottom zoom">
                                         <div class="card-body">
                                             <div class='d-flex px-0 px-lg-2 py-2 align-self-center'>
-                                                <i class="fas fa-check fa-3x fa-fw mt-2 text-white"></i>
+                                                <i class="fas fa-check fa-2x fa-fw mt-2 text-white"></i>
                                                 <div class='card-liner-content'>
                                                     <div class="media-body align-self-center text-white">
                                                         <span class="mb-0 h5 font-w-600 text-break">Funded Proposals(Approved)</span><br>
@@ -151,10 +165,10 @@
                                 </div>
 
                                 <div class="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
-                                    <div class="card cstRedColor">
+                                    <div class="card cstRedColor border-bottom border-success border-w-5 border-bottom zoom">
                                         <div class="card-body">
                                             <div class='d-flex px-0 px-lg-2 py-2 align-self-center'>
-                                                <i class="fas fa-times fa-3x fa-fw mt-2 text-white"></i>
+                                                <i class="fas fa-times fa-2x fa-fw mt-2 text-white"></i>
                                                 <div class='card-liner-content'>
                                                     <div class="media-body align-self-center text-white">
                                                         <span class="mb-0 h5 font-w-600 text-break">Funded Proposals(Rejected)</span><br>
@@ -166,10 +180,10 @@
                                     </div>
                                 </div>
                                 <div class="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
-                                    <div class="card cstRedColor">
+                                    <div class="card cstRedColor border-bottom border-success border-w-5 border-bottom zoom">
                                         <div class="card-body">
                                             <div class='d-flex px-0 px-lg-2 py-2 align-self-center'>
-                                                <i class="icon-grid fa-3x fa-fw mt-2 text-white"></i>
+                                                <i class="icon-grid fa-2x fa-fw mt-2 text-white"></i>
                                                 <div class='card-liner-content'>
                                                     <div class="media-body align-self-center text-white">
                                                         <span class="mb-0 h5 font-w-600">No. Of Events</span><br>
@@ -182,10 +196,10 @@
                                 </div>
 
                                 <div class="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
-                                    <div class="card cstRedColor">
+                                    <div class="card cstRedColor border-bottom border-success border-w-5 border-bottom zoom">
                                         <div class="card-body">
                                             <div class='d-flex px-0 px-lg-2 py-2 align-self-center'>
-                                                <i class="icon-grid fa-3x fa-fw mt-2 text-white"></i>
+                                                <i class="icon-grid fa-2x fa-fw mt-2 text-white"></i>
                                                 <div class='card-liner-content'>
                                                     <div class="media-body align-self-center text-white">
                                                         <span class="mb-0 h5 font-w-600">No. Of Internships</span><br>
